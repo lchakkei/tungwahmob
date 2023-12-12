70ec2b88e166900989f044550ff3a71c72d975a4
Jack Lee <jack.lee@work.twghintranet.org>
2023-12-12 11:53:23 +0800

    update aar

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,6 +36,7 @@ dependencies {
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation(project(mapOf("path" to ":tungwahmob")))
+    
 
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
